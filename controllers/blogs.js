36a0106dacd664658dac0ae946b715db9593ffd8
anketola <39833064+anketola@users.noreply.github.com>
2024-12-15 17:54:26 +0200

    Finish exercise 13.22

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -1,24 +1,7 @@
 const router = require('express').Router()
-const jwt = require('jsonwebtoken')
-const { SECRET } = require('../util/config')
 const { Op } = require('sequelize')
 const { Blog, User } = require('../models')
-
-const tokenExtractor = (req, res, next) => {
-  const authorization = req.get('authorization')
-  if (authorization && authorization.toLowerCase().startsWith('bearer ')) {
-    try {
-      console.log(authorization.substring(7))
-      req.decodedToken = jwt.verify(authorization.substring(7), SECRET)
-    } catch (error) {
-      console.log(error)
-      return res.status(401).json({ error: 'token invalid' })
-    }
-  } else {
-    return res.status(401).json({ error: 'token missing' })
-  }
-  next()
-}
+const { tokenExtractor } = require('../util/tokenExtractor')
 
 router.get('/', async (req, res) => {
   const where = {}
--- controllers/readinglists.js
+++ controllers/readinglists.js
@@ -1,6 +1,7 @@
 const router = require('express').Router()
 
 const { ReadingList } = require('../models')
+const { tokenExtractor } = require('../util/tokenExtractor')
 
 router.post('/', async (req, res) => {  
   const { blogId, userId } = req.body;
@@ -9,6 +10,19 @@ router.post('/', async (req, res) => {
     userId
   });  
   res.json(readingList)  
+})
+
+router.put('/:id', tokenExtractor, async (req, res) => {
+  const selectedBlog = await ReadingList.findByPk(req.params.id)
+  
+  if (selectedBlog.userId === req.decodedToken.id) {
+    if (req.body.read === true) {
+      selectedBlog.read = true
+      await selectedBlog.save()      
+    }
+  }
+  res.json(selectedBlog)
+
 })
 
 
